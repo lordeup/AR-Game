bb9933d9c2111811ccc663725b842494156333f6
Andrey Kalanov <32957572+lordeup@users.noreply.github.com>
2020-12-20 19:32:31 +0300

    fix plane position

--- Minotaur maze/Assets/Scripts/CustomTrackableEventHandler.cs
+++ Minotaur maze/Assets/Scripts/CustomTrackableEventHandler.cs
@@ -4,12 +4,10 @@ using Vuforia;
 public class CustomTrackableEventHandler : DefaultTrackableEventHandler
 {
     private GameController _gameController;
-    private PlaneFinderBehaviour _planeFinderBehaviour;
 
     private void Awake()
     {
         _gameController = FindObjectOfType<GameController>();
-        _planeFinderBehaviour = FindObjectOfType<PlaneFinderBehaviour>();
     }
 
     protected override void Start()
@@ -33,7 +31,6 @@ public class CustomTrackableEventHandler : DefaultTrackableEventHandler
     {
         if (!mTrackableBehaviour) return;
         _gameController.enabled = true;
-        _gameController.transform.localPosition = _planeFinderBehaviour.transform.localPosition;
 
         var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
         var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
