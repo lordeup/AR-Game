be1046fab2c4433088f0b1143bd120bb6537f83c
Reywam <reywam@mail.ru>
2020-12-20 20:10:50 +0300

    Add visibility around the player script

---
+++ Minotaur maze/Assets/Scripts/VisibilityAroundPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Вешаем на объект Rigidbody
+    // Вешаем на объект любой подходящий коллайдер(нам сойдёт куб), задаём его размеры так, чтобы он охватывал чуть больше чем ширину коридора лабиринта
+    // Ставим на коллайдере галку isTrigger
+    // Вешаем на объект этот скрипт.
+    // По итогу скрипт будет отображать все объекты, которые попадают в наш коллайдер-триггер
+    void OnTriggerExit(Collider other)
+    {
+        other.gameObject.GetComponent<Renderer>().enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Renderer>().enabled = true;
+    }
+}
